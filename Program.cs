e685163be4a62a3e37860f6091f7fcddd9d9023b
VVCephei <vladgol90@gmail.com>
2023-03-13 15:03:56 +0200

    task 27

--- Program.cs
+++ Program.cs
@@ -4,22 +4,22 @@ int numberA = Convert.ToInt32(Console.ReadLine());
 int SummNumbers(int num)
 {
     int summ = 0;
-    if(numberA > 0)
-    while (num % 10 > 0)
+    if (numberA > 0)
+        while (num % 10 > 0)
+        {
+            summ = summ + num % 10;
+            num = num / 10;
+        }
+    else
     {
-        summ = summ + num % 10;
-        num = num / 10;
+        num = num * -1;
+        while (num % 10 > 0)
+        {
+            summ = summ + num % 10;
+            num = num / 10;
+        }
     }
-   else
-{
-    num = num * -1;
-     while (num % 10 > 0)
-    {
-        summ = summ + num % 10;
-        num = num / 10; 
-    }
-}
-return summ;
+    return summ;
 }
 int summNumbers = SummNumbers(numberA);
 Console.WriteLine($"Сумма цифр числа {numberA} = {summNumbers}");
